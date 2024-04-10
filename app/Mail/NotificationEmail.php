48eb3f87b64ef145c1282fd1661d2e73dd4b7ca8
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-10 11:25:28 -0500

    se agrega mails

---
+++ app/Mail/NotificationEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\student;
+
+class NotificationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $notification;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(student $notification)
+    {
+        $this->notification = $notification;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.notification_email');
+    }
+}
